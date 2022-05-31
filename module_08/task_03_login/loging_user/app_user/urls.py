e1ab54f95cc094abb0d27bbe18cde27540aa840a
vm-viktor@mail.ru <vm021077vm>
2022-05-31 21:37:01 +0700

    Practical task_03 module_08 refactor file views.py

--- module_08/task_03_login/loging_user/app_user/urls.py
+++ module_08/task_03_login/loging_user/app_user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from app_user.views import AuthLoginView
-from loging_user.viesw import MainView
+from loging_user.views import MainView
 
 
 urlpatterns = [
--- module_08/task_03_login/loging_user/loging_user/urls.py
+++ module_08/task_03_login/loging_user/loging_user/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from loging_user.viesw import MainView
+from loging_user.views import MainView
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),

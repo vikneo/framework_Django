c4fe9bea341821eb2f6e56a7b7cfc7d03c8a302d
vm-viktor@mail.ru <vm021077vm>
2022-06-01 19:43:30 +0700

    Practical created task_04 module_08

---
+++ module_08/task_04/loging_user/app_user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app_user.views import AuthLoginView
+from loging_user.views import MainView
+
+
+urlpatterns = [
+    path('', MainView.as_view(), name='main'),
+    path('login/', AuthLoginView.as_view(), name='login'),
+]
